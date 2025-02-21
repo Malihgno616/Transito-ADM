d9c4f03f21d67e882b01b4cc4f1f87c62e00dd30
Ackin.Dev <ackinlino123@gmail.com>
2025-02-21 13:28:40 -0300

    Corrigindo erros ortográficos

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,7 +1,7 @@
 
 <footer class="footer">
   <p>&copy; Todos os direitos reservados.</p>
-  <p>Desenovido por <a href="#">Ackin.Dev</a></p>
+  <p>Desenvolvido por <a href="#">Ackin.Dev</a></p>
 </footer>
 </body>
 </html>
