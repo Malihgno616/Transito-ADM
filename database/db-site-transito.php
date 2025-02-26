b72d7692d72522f359e0b21451777233ce946ffa
Ackin.Dev <ackinlino123@gmail.com>
2025-02-26 12:39:50 -0300

    Exibindo as informações do banco de dados na tabela do site

---
+++ database/db-site-transito.php
@@ -0,0 +1,31 @@
+<?php 
+
+define('DB_HOST_form', 'localhost');
+define('DB_USER_form', 'root');
+define('DB_PASSWORD_form', '');
+define('DB_NAME_form', 'site_transito');
+
+class DbForms {
+  
+  public function queryForm($sql, $params=[]){
+    try {
+      $pdo = new PDO('mysql:host=' . DB_HOST_form . ';dbname=' . DB_NAME_form,
+      DB_USER_form, DB_PASSWORD_form);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute($params);
+
+      $restults = $stmt->fetchAll(PDO::FETCH_CLASS);
+      return [
+        'status' => 'success',
+        'data' => $restults
+      ];
+
+    } catch (PDOException $err) {
+      return [
+        'status' => 'error',
+        'data' => $err->getMessage()
+      ];
+    }
+  }
+}
