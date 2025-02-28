2081ac9ad8aa045b213f7ae1bdde3012f1ecebc0
Ackin.Dev <ackinlino123@gmail.com>
2025-02-28 08:46:29 -0300

    adicionando link do github

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,7 +1,6 @@
-
 <footer class="footer">
   <p>&copy; Todos os direitos reservados.</p>
-  <p>Desenvolvido por <a href="#">Ackin.Dev</a></p>
+  <p>Desenvolvido por <a target="_blank" href="https://github.com/Malihgno616">Ackin.Dev</a></p>
 </footer>
 </body>
 </html>
