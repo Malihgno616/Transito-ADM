ff416c5c31f1535b042a26205fcd65223a16d23e
Ackin.Dev <ackinlino123@gmail.com>
2025-03-11 11:53:33 -0300

    Estilizando o footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,15 @@
-<footer class="footer">
+<footer 
+class=
+"
+bg-gray-600 
+p-4 flex 
+justify-center 
+items-center 
+flex-col 
+text-2xl
+">
   <p>&copy; Todos os direitos reservados.</p>
-  <p>Desenvolvido por <a target="_blank" href="https://github.com/Malihgno616">Ackin.Dev</a></p>
+  <p>Desenvolvido por <a class="text-white hover:underline" target="_blank" href="https://github.com/Malihgno616">Ackin.Dev</a>.</p>
 </footer>
 </body>
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -31,17 +31,18 @@ if($response['status'] === 'success' && !empty($response['data'])) {
   <title>Transito ADM</title>
   <!-- <link rel="stylesheet" href="../assets/styles/global.css">
   <link rel="stylesheet" href="../assets/styles/header.css"> -->
+  <!-- <link rel="stylesheet" href="../assets/styles/footer.css"> -->
   <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../assets/styles/tela-inicial.css">
-  <link rel="stylesheet" href="../assets/styles/footer.css">
   <link rel="stylesheet" href="../assets/styles/tela-servicos.css">
   <link rel="stylesheet" href="../assets/styles/table.css">
+  
+  <link href="../src/output.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
-  <link href="../src/output.css" rel="stylesheet">
 </head>
 <body>
 <header class="bg-yellow-400 drop-shadow-md">
