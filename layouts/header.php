3b0a46023d183cbeb2d39acdc5d462b473025da4
Ackin.Dev <ackinlino123@gmail.com>
2025-03-11 11:24:02 -0300

    Testando tailwindcss no header

--- layouts/header.php
+++ layouts/header.php
@@ -29,8 +29,8 @@ if($response['status'] === 'success' && !empty($response['data'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Transito ADM</title>
-  <link rel="stylesheet" href="../assets/styles/global.css">
-  <link rel="stylesheet" href="../assets/styles/header.css">
+  <!-- <link rel="stylesheet" href="../assets/styles/global.css">
+  <link rel="stylesheet" href="../assets/styles/header.css"> -->
   <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../assets/styles/tela-inicial.css">
@@ -41,18 +41,20 @@ if($response['status'] === 'success' && !empty($response['data'])) {
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
+  <link href="../src/output.css" rel="stylesheet">
 </head>
 <body>
-<header class="header">
-  <nav class="nav-menu">
+<header class="bg-yellow-400 drop-shadow-md ">
+  <nav class="flex justify-around items-center p-2 text-2xl ">
       <a href="index.php?rota=home"><img src="../assets/img/logo-borda-branca.png" width="200px" alt="logo transito"></a>
-      <a href="index.php?rota=home">Home</a>
-      <a href="index.php?rota=servicos">Serviços</a>
-      <a href="index.php?rota=contatos">Contatos</a>
-      <a href="index.php?rota=notificacoes">Todas as notificações</a>
-      <a class="logout" href="index.php?rota=logout">Sair <abbr title="Sair"><i class="fa-solid fa-arrow-right-from-bracket"></i></abbr></a>  
+      <a class="hover:text-gray-100 duration-200" href="index.php?rota=home">Home</a>
+      <a class="hover:text-gray-100 duration-200" href="index.php?rota=servicos">Serviços</a>
+      <a class="hover:text-gray-100 duration-200" href="index.php?rota=contatos">Contatos</a>
+      <a class="hover:text-gray-100 duration-200" href="index.php?rota=notificacoes">Todas as notificações</a>
+      <a class="w-25 bg-gray-800 text-white p-1 flex justify-center items center rounded-md hover:bg-gray-200 hover:text-gray-600 duration-200" href="index.php?rota=logout">Sair <abbr title="Sair"> <i class="fa-solid fa-arrow-right-from-bracket"></i></abbr></a>  
   </nav> 
 </header>
 <div class="user-name">
-  <span><h2>Bem-vindo, <?= htmlspecialchars($nome) ?? ""; ?></h2></span>
+  <span><h2 class="text-3xl font-bold underline">Bem-vindo, <?= htmlspecialchars($nome) ?? ""; ?></h2></span>
 </div>
+
