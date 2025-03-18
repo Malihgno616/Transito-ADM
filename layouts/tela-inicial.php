b5da971fb9e527533a20f5bb1fd933a887852773
Ackin.Dev <ackinlino123@gmail.com>
2025-03-18 11:35:27 -0300

    Responsividade na tela inicial do site administrativo

--- layouts/tela-inicial.php
+++ layouts/tela-inicial.php
@@ -1,31 +1,53 @@
-<main class="animate__animated animate__fadeIn p-5 rounded-md border w-200 border-gray-300 mx-auto mb-25">
+<main class="animate__animated 
+      animate__fadeIn
+      p-5 
+      rounded-md 
+      border 
+      w-200 
+      border-gray-300 
+      mx-auto 
+      mb-25
+      max-lg:w-150
+      max-lg:p-1
+      max-md:w-140
+      max-sm:w-85
+      ">
 
-  <h1 class="text-5xl text-center">Tela Inicial - Administrativo</h1>
+  <h1 class="text-5xl 
+      text-center 
+      max-lg:text-3xl 
+      max-md:text-2xl"
+  >
+  Tela Inicial - Administrativo
+  </h1>
   
     <div 
-    class="
-    p-5 
-    mb-3 
-    grid 
-    grid-cols-1 
-    gap-5 
-    w-2/3 
-    mx-auto">
-
+    class="p-5 
+           mb-3 
+           grid 
+           grid-cols-1 
+           gap-5 
+           w-2/3 
+           mx-auto
+           max-lg:w-full
+           max-md:w-3/4  
+           max-sm:w-full
+           max-sm:p-3"
+      >
       <div class="bg-gray-200 p-10 rounded flex justify-center items-center text-center hover:shadow-lg duration-200">
-        <a class="text-4xl" href="index.php?rota=servicos"><h2>Serviços</h2><i class="fa-solid fa-briefcase"></i></a>
+        <a class="text-4xl max-lg:text-2xl" href="index.php?rota=servicos"><h2>Serviços</h2><i class="fa-solid fa-briefcase"></i></a>
       </div>
     
       <div class="bg-gray-200 p-10 rounded flex justify-center items-center text-center hover:shadow-lg duration-200">
-        <a class="text-4xl" href="index.php?rota=usuarios"><h2>Gerenciar Usuários</h2><i class="fa-solid fa-circle-user"></i></a>
+        <a class="text-4xl max-lg:text-2xl" href="index.php?rota=usuarios"><h2>Gerenciar Usuários</h2><i class="fa-solid fa-circle-user"></i></a>
       </div>
     
       <div class="bg-gray-200 p-10 rounded flex justify-center items-center text-center hover:shadow-lg duration-200">
-        <a class="text-4xl" href="index.php?rota=notificacoes"><h2>Notificações</h2><i class="fa-solid fa-bell"></i></a>
+        <a class="text-4xl max-lg:text-2xl" href="index.php?rota=notificacoes"><h2>Notificações</h2><i class="fa-solid fa-bell"></i></a>
       </div>
     
       <div class="bg-gray-200 p-10 rounded flex justify-center items-center text-center hover:shadow-lg duration-200">
-        <a class="text-4xl" href="index.php?rota=contatos"><h2>Contatos</h2><i class="fa-solid fa-phone"></i></a>
+        <a class="text-4xl max-lg:text-2xl" href="index.php?rota=contatos"><h2>Contatos</h2><i class="fa-solid fa-phone"></i></a>
       </div>
             
     </div>
