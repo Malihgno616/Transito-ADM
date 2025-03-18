08f29007ad21af49177704663b81dc09852f7081
Ackin.Dev <ackinlino123@gmail.com>
2025-03-18 11:48:02 -0300

    responsividade no footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,15 +1,15 @@
 <footer 
-class=
-"
-bg-gray-600 
-p-4 flex 
-justify-center 
-items-center 
-flex-col 
-text-2xl
-">
+class="bg-gray-600 
+         p-4 flex 
+         justify-center 
+         items-center 
+         flex-col 
+         text-2xl
+         max-sm:p-2
+         max-sm:text-lg"
+         >
   <p>&copy; Todos os direitos reservados.</p>
-  <p>Desenvolvido por <a class="text-white hover:underline" target="_blank" href="https://github.com/Malihgno616">Ackin.Dev</a>.</p>
+  <p>Desenvolvido por <a class="text-white hover:underline " target="_blank" href="https://github.com/Malihgno616">Ackin.Dev</a>.</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
