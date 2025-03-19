8722a583492ab7b058dbaaf0a321717f8514de9d
Ackin.Dev <ackinlino123@gmail.com>
2025-03-19 11:40:49 -0300

    Adicionando estilos da tabela de notificação

--- layouts/table-notificacao.php
+++ layouts/table-notificacao.php
@@ -1,6 +1,51 @@
-<main class="main-table animate__animated animate__fadeIn">
-  <h1>Notificações</h1>
-  <table>
+<main class="main-table animate__animated animate__fadeIn p-4 
+rounded-md 
+w-300 
+border-gray-300 
+mx-auto 
+mb-25
+max-md:w-150
+max-sm:w-90
+max-md:text-md
+lg:p-6 
+lg:w-400 
+lg:border-gray-400 
+lg:mx-0 
+lg:mb-0">
+  <h1 class="text-5xl 
+  text-center 
+  p-5 
+  text-gray-900
+  max-md:text-2xl
+  ">Notificações</h1>
+  <button class="p-4 
+  text-2xl
+text-white 
+  rounded-md 
+  cursor-pointer 
+  bg-gray-800 
+  hover:bg-gray-600 
+  w-40 
+  h-10 
+  flex 
+  justify-center 
+  items-center 
+  m-auto 
+  duration-100
+  max-md:text-xl
+  max-md:w-20
+  max-md:h-5
+  "
+  onclick="window.location.href='index.php?rota=home'">Voltar</button>
+  <table class="table-auto 
+  mx-auto 
+  mb-5 
+  border-separate 
+  border-spacing-10 
+  text-2xl
+  max-md:text-xl
+  max-md:w-150
+  " >
     <thead>
       <tr>
         <th>Id</th>
@@ -41,9 +86,7 @@
       <td data-label="Categoria">${notification.category}</td>
       <td data-label="Data">${notification.date}</td>
       <td data-label="Ações">
-        <button class="edit-btn" title="Editar">Editar</button>
-        <button class="view-btn" title="Visualizar">Visualizar</button>
-        <button class="delete-btn" title="Excluir">Excluir</button>
+        <button class='border-2 border-red-500 rounded-sm p-2 text-red-500 hover:dark:bg-red-500 hover:text-white duration-150 cursor-pointer' title="Excluir">Excluir</button>
       </td>
     `;
     tableBody.appendChild(row);
