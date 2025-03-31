99adec0aebc05f77ce8a47279764a4686dbf0d6d
Ackin.Dev <ackinlino123@gmail.com>
2025-03-31 13:23:08 -0300

    corrigindo o erro de visibilidade dos estados do beneficiário

--- layouts/modals/modalDeficiente.php
+++ layouts/modals/modalDeficiente.php
@@ -70,11 +70,11 @@
                   </div>
                   <div class="relative z-0 w-full mb-5 group">
                       <select id="uf-beneficiario" class="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
-                        <option value=""></option>
-                       <?php foreach($estados as $uf): ?>
-                        <option value="<?= $uf ?>" <?= ($uf == $uf_beneficiario) ? 'selected' : '' ?>>
-                            <?= $uf ?>
-                        </option>
+                        <option value=""></option>                    
+                        <?php foreach($estados as $uf): ?>
+                            <option value="<?= $uf ?>" <?= ($uf == $uf_beneficiario) ? 'selected' : ''?>>
+                                <?= $uf ?>
+                            </option>
                         <?php endforeach; ?>
                       </select>
                       <label for="uf-idoso" class="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Estado(UF)</label>  
